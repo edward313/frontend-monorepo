f4d56b247e4e227543cf2a96754ace2921c24301
Edward <phatluu2929@gmail.com>
2026-02-19 22:49:35 +0700

    no message

--- apps/setting/src/app/page.tsx
+++ apps/setting/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@repo/ui/button";
-import { useState } from "react";
+import { type ChangeEvent, type FormEvent, useState } from "react";
 
 const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:3001";
 
@@ -9,6 +9,22 @@ export default function Web() {
 	const [response, setResponse] = useState<{ message: string } | null>(null);
 	const [error, setError] = useState<string | undefined>();
 
+	const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+		setName(e.target.value);
+
+	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		try {
+			const result = await fetch(`${API_HOST}/message/${name}`);
+			const response = await result.json();
+			setResponse(response);
+		} catch (err) {
+			console.error(err);
+			setError("Unable to fetch response");
+		}
+	};
+
 	const onReset = () => {
 		setName("");
 	};
@@ -16,9 +32,15 @@ export default function Web() {
 	return (
 		<div>
 			<h1>Web</h1>
-			<form>
+			<form onSubmit={onSubmit}>
 				<label htmlFor="name">Name </label>
-				<input type="text" name="name" id="name" value={name}></input>
+				<input
+					type="text"
+					name="name"
+					id="name"
+					value={name}
+					onChange={onChange}
+				></input>
 				<Button type="submit">Submit</Button>
 			</form>
 			{error && (
