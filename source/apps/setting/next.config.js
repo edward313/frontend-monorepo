4ec88ab03e62ebc6e51846b65d602b8e1e0320ae
Edward <phatluu2929@gmail.com>
2026-02-19 22:04:46 +0700

    chore: add pnpm workspace configuration and turbo.json

--- source/apps/setting/next.config.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["@repo/ui"],
-  output: "standalone",
-  outputFileTracingRoot: path.join(__dirname, "../../"),
-};
