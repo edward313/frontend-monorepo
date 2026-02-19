af655f8bdbb71ee55385482f8d9a0249ee912246
Edward <phatluu2929@gmail.com>
2026-02-19 22:48:26 +0700

    update biome-config

--- apps/setting/src/app/page.tsx
+++ apps/setting/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@repo/ui/button";
-import { type ChangeEvent, type FormEvent, useEffect, useState } from "react";
+import { useState } from "react";
 
 const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:3001";
 
@@ -9,27 +9,6 @@ export default function Web() {
 	const [response, setResponse] = useState<{ message: string } | null>(null);
 	const [error, setError] = useState<string | undefined>();
 
-	useEffect(() => {
-		setResponse(null);
-		setError(undefined);
-	}, [name]);
-
-	const onChange = (e: ChangeEvent<HTMLInputElement>) =>
-		setName(e.target.value);
-
-	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-
-		try {
-			const result = await fetch(`${API_HOST}/message/${name}`);
-			const response = await result.json();
-			setResponse(response);
-		} catch (err) {
-			console.error(err);
-			setError("Unable to fetch response");
-		}
-	};
-
 	const onReset = () => {
 		setName("");
 	};
@@ -37,15 +16,9 @@ export default function Web() {
 	return (
 		<div>
 			<h1>Web</h1>
-			<form onSubmit={onSubmit}>
+			<form>
 				<label htmlFor="name">Name </label>
-				<input
-					type="text"
-					name="name"
-					id="name"
-					value={name}
-					onChange={onChange}
-				></input>
+				<input type="text" name="name" id="name" value={name}></input>
 				<Button type="submit">Submit</Button>
 			</form>
 			{error && (
